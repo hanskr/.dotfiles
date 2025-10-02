923315d2724e572bc29b3e36d43a01f212c6a15b
Hans Kristian Kismul <hans.kristian.kismul@finn.no>
2025-10-02 14:22:03 +0200

    gcloud

--- hm/work.nix
+++ hm/work.nix
@@ -2,6 +2,12 @@
 {
   home.packages = with pkgs; [
     asdf-vm
+    (google-cloud-sdk.withExtraComponents (
+      with google-cloud-sdk.components;
+      [
+        gke-gcloud-auth-plugin
+      ]
+    ))
     k6
     kubectl
     kubetail
